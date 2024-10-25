fa09d77a60c953904d44098840070dc77e4ea1ca
Isaac Freires Santos <isaacfreires@gmail.com>
2024-10-25 17:32:45 -0300

    ✨ feat: Adicionado file_uploader

--- analise.py
+++ analise.py
@@ -1,4 +1,16 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from io import StringIO
 
+base_csv = st.file_uploader("Escolha as 2 bases para análise", accept_multiple_files=True, type='csv')
+
+if base_csv != None:
+    for file in base_csv:
+        bytes_data = base_csv.getvalue()
+        string_data = StringIO(bytes_data.decode("UTF-8"))
+        base = pd.read_csv(string_data)
+
+        st.header("Prévia dos dados - {file.name}")
+        base
+    
