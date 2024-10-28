21acb4b0056ed026cedf5485b93eec2aae7bb8fd
Isaac Freires <isaacfreires@gmail.com>
2024-10-28 15:53:37 -0300

    ✨ feat: Feito merge das bases e adicionado tabela de Classificação e Vendas Globais. Adicionado filtro para todos os anos

--- analise.py
+++ analise.py
@@ -10,39 +10,75 @@ base_csv = st.file_uploader("Escolha as 2 bases para análise", accept_multiple_
 base_vgsales = "vgsales.csv"
 base_games = "games.csv"
 
-if base_csv != None:
+# Inicializa as variáveis das bases
+df_vgsales = None
+df_games = None
+
+col1, col2 = st.columns(spec=2)
+
+# Carrega os arquivos
+if base_csv:
     for file in base_csv:
         bytes_data = file.getvalue()
         string_data = StringIO(bytes_data.decode("UTF-8"))
         base = pd.read_csv(string_data)
 
-        st.header(f"Prévia dos dados - {file.name}")
-        base
-
-    # Pegando coluna data da base vgsales
-    if file.name == base_vgsales:
-        base['Year'] = pd.to_datetime(base['Year'], format='%Y', errors='coerce')
+        if file.name == base_vgsales:
+            df_vgsales = base
+            df_vgsales['Year'] = pd.to_datetime(df_vgsales['Year'], format='%Y', errors='coerce')
+            with col1:
+                st.header(f"Prévia dos dados - {file.name}")
+                st.write(df_vgsales.head())
         
-        if base['Year'].isnull().any():
-            st.write("Alguns não poderam ser convertidos")
+        elif file.name == base_games:
+            df_games = base
+            with col2:
+                st.header(f"Prévia dos dados - {file.name}")
+                st.write(df_games.head())
 
-        # Extrair anos
-        anos = base['Year'].dt.to_period('Y').unique()
+    # Verifica se ambas as bases foram carregadas
+    if df_vgsales is not None and df_games is not None:
+        # Filtra dados que possuem valores de "Year" não nulos na base vgsales
+        df_vgsales = df_vgsales.dropna(subset=['Year'])
+        
+        
+        anos = df_vgsales['Year'].dt.to_period('Y').unique()
+        anos = ["Todos os anos"] + list(anos.astype(str))
 
         st.sidebar.header("Filtros")
-
-        # Filtro anos
         ano_selecionado = st.sidebar.selectbox(
             "Ano",
-            anos.astype(str),
-            index=None,
+            anos,
+            index=0,
             placeholder="Selecione o ano"
         )
 
-        base_filtrada_vg = base[base['Year'].dt.to_period('Y').astype(str) == ano_selecionado]
+        if ano_selecionado == "Todos os anos":
+            df_vgsales_filtrada = df_vgsales
+        else:
+            # Filtra os dados pelo ano selecionado na base vgsales
+            df_vgsales_filtrada = df_vgsales[df_vgsales['Year'].dt.to_period('Y').astype(str) == ano_selecionado]
+
+        # Faz a fusão das bases usando as colunas de nome (Name e Title)
+        df_fusionada = pd.merge(
+            df_vgsales_filtrada,
+            df_games,
+            how='inner',
+            left_on='Name',
+            right_on='Title'
+        )
+
+        # 
+        st.write("Dados combinados para o ano selecionado:")
+        st.write(df_fusionada[['Year', 'Name', 'Global_Sales', 'Title']])
         
-        # Agrupando ano e nome da base vgsales
-        data_nome = base_filtrada_vg.groupby(['Year', 'Name']).reset()
+        # Vendas globais
+        fig_vendas_globais = px.bar(df_fusionada, x='Title', y='Global_Sales', title=f"Vendas Globais dos Jogos - {ano_selecionado} - (Em Milhões)")
+        st.plotly_chart(fig_vendas_globais)
 
+        # Rating com plotly
+        fig_aval_media = px.bar(df_fusionada, x='Name', y='Rating', title="Classificação do jogos")
+        st.plotly_chart(fig_aval_media)
 
-        st.write(data_nome)
+        # Rating com stream
+        st.bar_chart(df_fusionada, x="Name", y="Rating") 
